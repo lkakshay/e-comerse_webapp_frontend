2eed64cec0a249b113e8da44aa16a6140d6379a6
akshaylk <connectme.lkakshay@gmail.com>
2022-08-24 01:41:13 +0530

    product details ui completed

--- src/Pages/singleproduct.jsx
+++ src/Pages/singleproduct.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
 import Button from "@mui/material/Button";
@@ -7,7 +12,11 @@ import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
 import { Rating } from "@mui/material";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { borderRadius } from "@mui/system";
 
+let data = new Array(4).fill(0);
 export const Singleproduct = () => {
   return (
     <React.Fragment>
@@ -32,11 +41,8 @@ export const Singleproduct = () => {
         </Grid>
         <Grid container xs={12} md={12} sm={12} lg={6} sx={{ py: 1, px: 1 }}>
           <Box sx={{ width: "100%", p: "3%", bgcolor: "background.paper" }}>
-            <Box>
-              <Typography
-                gutterBottom
-                sx={{ fontSize: "22px", fontWeight: "900" }}
-              >
+            <Box sx={{ px: 2, py: 1 }}>
+              <Typography gutterBottom variant="h4">
                 Toothbrush
               </Typography>
 
@@ -54,43 +60,104 @@ export const Singleproduct = () => {
                   ₹ 356
                 </Typography>
                 <Typography
-                sx={{ fontSize: "16px", px: 1, py: 2 ,color:"green"}}>
+                  sx={{ fontSize: "16px", px: 1, py: 2, color: "green" }}
+                >
                   20%
-                  </Typography>
+                </Typography>
               </Box>
-
-              
             </Box>
-            <Box sx={{p:1}}>
-            <Divider variant="middle" sx={{mb:2}} />
-              <Typography gutterBottom color="text.secondary" variant="body2">
-              Bank Offer10% off on SBI Credit Card, up to ₹1500. On orders of ₹5000 and aboveT&C
-              </Typography>
-              <Typography gutterBottom color="text.secondary" variant="body2">
-              Bank Offer10% off on SBI Credit Card EMI Transactions, up to ₹2000. On orders of ₹5000 and aboveT&C
-              </Typography>
-              <Typography gutterBottom color="text.secondary" variant="body2">
-              Bank Offer5% Cashback on Flipkart Axis Bank CardT&C
-              </Typography>
-              <Typography gutterBottom color="text.secondary" variant="body2">
-              No cost EMI ₹1,391/month. Standard EMI also available
-              </Typography>
+            <Box sx={{ p: 1 }}>
+              <Divider variant="middle" sx={{ mb: 2 }} />
 
-            <Divider variant="middle" sx={{mt:2}} />
+              <div style={{ display: "flex" }}>
+                <LocalOfferIcon
+                  fontSize="small"
+                  sx={{ color: "#00d039", px: 1 }}
+                />
+                <Typography gutterBottom color="text.secondary" variant="body2">
+                  Bank Offer10% off on SBI Credit Card, up to ₹1500. On orders
+                  of ₹5000 and aboveT&C
+                </Typography>
+              </div>
+              <div style={{ display: "flex" }}>
+                <LocalOfferIcon
+                  fontSize="small"
+                  sx={{ color: "#00d039", px: 1 }}
+                />
+                <Typography gutterBottom color="text.secondary" variant="body2">
+                  Bank Offer10% off on SBI Credit Card EMI Transactions, up to
+                  ₹2000. On orders of ₹5000 and aboveT&C
+                </Typography>
+              </div>
+              <div style={{ display: "flex" }}>
+                <LocalOfferIcon
+                  fontSize="small"
+                  sx={{ color: "#00d039", px: 1 }}
+                />
+                <Typography gutterBottom color="text.secondary" variant="body2">
+                  Bank Offer5% Cashback on Flipkart Axis Bank CardT&C
+                </Typography>
+              </div>
+              <div style={{ display: "flex" }}>
+                <LocalOfferIcon
+                  fontSize="small"
+                  sx={{ color: "#00d039", px: 1 }}
+                />
+                <Typography gutterBottom color="text.secondary" variant="body2">
+                  No cost EMI ₹1,391/month. Standard EMI also available
+                </Typography>
+              </div>
+
+              <Divider variant="middle" sx={{ mt: 2 }} />
             </Box>
             <Box sx={{ m: 2 }}>
-              <Typography gutterBottom variant="body1">
-                Select type
-              </Typography>
-              <Stack direction="row" spacing={1}>
-                <Chip label="Extra Soft" />
-                <Chip color="primary" label="Soft" />
-                <Chip label="Medium" />
-                <Chip label="Hard" />
-              </Stack>
+              <FormControl>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  {data?.map((el, i) => (
+                    <FormControlLabel
+                      value={i}
+                      control={<Radio size="medium" color="success" />}
+                      label="Female"
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </Box>
+
+            <Box sx={{ m: 2 }}>
+              <Button
+                variant="outline"
+                sx={{borderRadius:'0px' ,px:0 , fontSize:'16px',fontWeight:"900"}}
+              >
+                -
+              </Button>
+              <Button
+              variant="outline"
+               sx={{borderRadius:'0px' ,px:0 , fontSize:'16px',fontWeight:"900"}}>
+                1</Button>
+              <Button
+              variant="outline"
+               sx={{borderRadius:'0px' ,px:0 , fontSize:'16px',fontWeight:"900"}}>
+                +</Button>
             </Box>
             <Box sx={{ mt: 3, m: 1, mb: 1 }}>
-              <Button>Add to cart</Button>
+              <Button
+                sx={{ mx: 1, py: 1, backgroundColor: "#00081c" }}
+                variant="contained"
+              >
+                {" "}
+                Buy Now
+              </Button>
+              <Button
+                sx={{ my: 2, py: 1, backgroundColor: "#00081c" }}
+                variant="contained"
+              >
+                Add to cart
+              </Button>
             </Box>
           </Box>
         </Grid>
