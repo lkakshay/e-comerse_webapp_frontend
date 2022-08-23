fcbce550e3d29cc4b19af24b48f5cdd8e567d42c
akshaylk <connectme.lkakshay@gmail.com>
2022-08-23 23:19:32 +0530

    updated single product page

--- src/Pages/singleproduct.jsx
+++ src/Pages/singleproduct.jsx
@@ -21,14 +21,18 @@ export const Singleproduct = () => {
           sx={{
             my: 1,
             px: 1,
-            backgroundColor: "white",
-            height: "300px",
-            border: "2px solid black",
           }}
-        ></Grid>
-        <Grid container xs={12} md={12} sm={12} lg={6} sx={{ my: 1, pl: 1 }}>
-          <Box sx={{ width: "100%", p: 2, bgcolor: "background.paper" }}>
-            <Box sx={{ my: 1, mx: 1 }}>
+        >
+          <div
+            style={{
+              backgroundColor: "white",
+              height: "300px",
+            }}
+          ></div>
+        </Grid>
+        <Grid container xs={12} md={12} sm={12} lg={6} sx={{ py: 1, px: 1 }}>
+          <Box sx={{ width: "100%", p: "3%", bgcolor: "background.paper" }}>
+            <Box>
               <Typography
                 gutterBottom
                 sx={{ fontSize: "22px", fontWeight: "900" }}
@@ -43,13 +47,37 @@ export const Singleproduct = () => {
                 readOnly
                 size="medium"
               />
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  sx={{ fontSize: "26px", fontWeight: "900", px: 1, py: 2 }}
+                >
+                  ₹ 356
+                </Typography>
+                <Typography
+                sx={{ fontSize: "16px", px: 1, py: 2 ,color:"green"}}>
+                  20%
+                  </Typography>
+              </Box>
 
-              <Typography color="text.secondary" variant="body2">
-                Pinstriped cornflower blue cotton blouse takes you on a walk to
-                the park or just down the hall.
-              </Typography>
+              
             </Box>
-            <Divider variant="middle" />
+            <Box sx={{p:1}}>
+            <Divider variant="middle" sx={{mb:2}} />
+              <Typography gutterBottom color="text.secondary" variant="body2">
+              Bank Offer10% off on SBI Credit Card, up to ₹1500. On orders of ₹5000 and aboveT&C
+              </Typography>
+              <Typography gutterBottom color="text.secondary" variant="body2">
+              Bank Offer10% off on SBI Credit Card EMI Transactions, up to ₹2000. On orders of ₹5000 and aboveT&C
+              </Typography>
+              <Typography gutterBottom color="text.secondary" variant="body2">
+              Bank Offer5% Cashback on Flipkart Axis Bank CardT&C
+              </Typography>
+              <Typography gutterBottom color="text.secondary" variant="body2">
+              No cost EMI ₹1,391/month. Standard EMI also available
+              </Typography>
+
+            <Divider variant="middle" sx={{mt:2}} />
+            </Box>
             <Box sx={{ m: 2 }}>
               <Typography gutterBottom variant="body1">
                 Select type
@@ -61,7 +89,7 @@ export const Singleproduct = () => {
                 <Chip label="Hard" />
               </Stack>
             </Box>
-            <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
+            <Box sx={{ mt: 3, m: 1, mb: 1 }}>
               <Button>Add to cart</Button>
             </Box>
           </Box>
